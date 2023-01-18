3c0e3c02d2bf37d1268bbcb48cec3aaaa8cf2f5c
Garrett Prentice <78630000+S1robe@users.noreply.github.com>
2023-01-17 22:32:14 -0800

    Update install.sh

--- vBox/install.sh
+++ vBox/install.sh
@@ -26,13 +26,13 @@ cd $VMDIR
 
 # install required software
 #---------------------------------------------------------
-#apt-get install -y virtualbox
+apt-get install -y virtualbox
 
-#wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 
-#echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
 
-#apt update && apt install -y terraform
+apt update && apt install -y terraform
 #----------------------------------------------------------
 
 # Download necessary files
